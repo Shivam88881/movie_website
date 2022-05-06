d260840d8a255a36fb23c1904f1a9ad2f850bcd3
Shivam88881 <shivam88881@gmail.com>
2022-05-06 22:09:31 +0530

    navbar updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,10 +3,15 @@ import {Link} from 'react-router-dom'
 export default class Navbar extends Component {
   render() {
     return (
-      <div class='navbar'>
-        <Link to='/movie_website' style={{textDecoration:'none'}}> <h2>Movies Website</h2></Link>
-        <Link to='/movie_website/favourites' style={{textDecoration:'none'}}> <h3 style={{marginLeft:'2rem'}}>Favroite</h3> </Link>
-      </div>
+        <>
+          <nav className="navbar navbar-dark bg-dark">
+            <div class='navbar'>
+            <Link to='/movie_website' style={{textDecoration:'none'}}> <h2>Movies Website</h2></Link>
+            <Link to='/movie_website/favourites' style={{textDecoration:'none'}}> <h3 style={{marginLeft:'2rem'}}>Favroite</h3> </Link>
+            </div>
+          </nav>
+        </>
+      
     )
   }
 }
